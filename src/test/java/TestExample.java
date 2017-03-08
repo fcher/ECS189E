a451052d517e3e1f2c317659b083db1f6196c028
Faraz Cherukattil <fcher@ucdavis.edu>
2017-03-08 14:20:36 -0800

    3 tests

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -28,4 +28,28 @@ public class TestExample {
         this.admin.createClass("Test", 2016, "Instructor", 15);
         assertFalse(this.admin.classExists("Test", 2016));
     }
+
+    @Test
+    public void testCap() {
+        this.admin.createClass("Test", 2017, "Instructor", -1);
+        assertFalse(this.admin.getClassCapacity("Test", 2017) <= 0);
+    }
+
+    @Test
+    public void testInst() {
+        int no_of_courses = 0;
+        this.admin.createClass("60", 2017, "Devanbu", 50);
+        this.admin.createClass("50", 2017, "Devanbu", 50);
+        this.admin.createClass("40", 2017, "Devanbu", 50);
+        if (this.admin.getClassInstructor("40",2017).equals("Devanbu"))
+            no_of_courses++;
+        if (this.admin.getClassInstructor("50",2017).equals("Devanbu"))
+            no_of_courses++;
+        if (this.admin.getClassInstructor("60",2017).equals("Devanbu"))
+            no_of_courses++;
+        assertFalse(no_of_courses > 2);
+    }
+
+
 }
+
