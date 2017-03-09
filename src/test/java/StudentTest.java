bfc2bacc0b38d138b043c932deb4d7b8ebe9e833
Faraz Cherukattil <fcher@ucdavis.edu>
2017-03-08 22:46:02 -0800

    Final

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -29,7 +29,7 @@ public class StudentTest {
     public void testRegister_1(){
         this.admin.createClass("60", 2017,"Devanbu",10);
         this.student.registerForClass("Tom","50", 2017);
-        assertTrue(this.student.isRegisteredFor("Tom","50",2017));
+        assertFalse(this.student.isRegisteredFor("Tom","50",2017));
     }
 
     @Test
@@ -37,6 +37,13 @@ public class StudentTest {
         this.admin.createClass("60", 2017,"Devanbu",0);
         this.student.registerForClass("Tom","60", 2017);
         assertFalse(this.student.isRegisteredFor("Tom","60",2017));
+    }
+
+    @Test
+    public void testRegister(){
+        this.admin.createClass("60", 2017,"Devanbu",30);
+        this.student.registerForClass("Tom","60", 2017);
+        assertTrue(this.student.isRegisteredFor("Tom","60",2017));
     }
 
     @Test
@@ -52,6 +59,14 @@ public class StudentTest {
         this.student.registerForClass("Tom","Test", 2016);
         this.student.dropClass("Tom","Test",2016);
         assertTrue(this.student.isRegisteredFor("Tom","Test",2016));
+    }
+
+    @Test
+    public void testDrop(){
+        this.admin.createClass("Test", 2017, "Instructor", 20);
+        this.student.registerForClass("Tom","Test", 2017);
+        this.student.dropClass("Tom","Test",2017);
+        assertFalse(this.student.isRegisteredFor("Tom","Test",2017));
     }
 
     @Test
@@ -77,6 +92,15 @@ public class StudentTest {
         this.student.registerForClass("Tom","Test", 2017);
         this.student.submitHomework("Tom","hw1","ans","Test", 2017);
         assertFalse(this.student.hasSubmitted("Tom","hw1","Test",2017));
+    }
+
+    @Test
+    public void testhw() {
+        this.admin.createClass("Test", 2017,"Instructor", 30);
+        this.student.registerForClass("Tom","Test", 2017);
+        this.prof.addHomework("Instructor", "Test", 2017, "hw1", "descr");
+        this.student.submitHomework("Tom","hw1","ans","Test", 2017);
+        assertTrue(this.student.hasSubmitted("Tom","hw1","Test",2017));
     }
 
 }
